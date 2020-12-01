1380f4d7fff8bd60bad8e187cf4fb862621052c7
Иван Викторович <help.venanen@yandex.ru>
2020-12-01 18:53:54 +0300

    create register layout

---
+++ components/BgContainer.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div id="form-image-bg" :class="{ 'more-blur': moreBlur }"></div>
+    <div class="container-fluid form-container">
+      <div class="row h100 middle-xs center-xs">
+        <div class="col" @mouseenter="setMoreBlur" @mouseleave="setLessBlur">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BgContainer',
+  data: () => ({
+    moreBlur: false,
+  }),
+  methods: {
+    setMoreBlur() {
+      this.moreBlur = true
+    },
+    setLessBlur() {
+      this.moreBlur = false
+    },
+  },
+}
+</script>
+
+<style scoped>
+.form-container {
+  height: 100vh;
+  width: 100vw;
+}
+
+#form-image-bg {
+  position: fixed;
+  z-index: -1;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-image: url('~assets/images/auth_bg.jpg');
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  filter: blur(2px);
+  transition: 0.2s;
+}
+#form-image-bg.more-blur {
+  filter: blur(3px);
+}
+input {
+  text-align: start !important;
+}
+
+.ant-card {
+  box-shadow: 4px 3px 15px 0px black;
+  border-radius: 10px;
+  transition: 0.2s;
+}
+.ant-card:hover {
+  box-shadow: 8px 8px 17px 0px black;
+  /*transform: scale(1.01);*/
+}
+</style>
--- pages/SignUp.vue
+++ pages/SignUp.vue
@@ -1,7 +1,7 @@
 <template>
   <bg-container>
-    <a-card title="Авторизация">
-      <NuxtLink slot="extra" to="signup">Регистрация</NuxtLink>
+    <a-card title="Регистрация">
+      <NuxtLink slot="extra" to="auth">Авторизация</NuxtLink>
       <a-form-model
         label-align="left"
         :model="form"
@@ -11,6 +11,14 @@
         <a-form-model-item label="Логин:">
           <a-input
             v-model="form.login"
+            size="large"
+            placeholder="Введите логин"
+          />
+        </a-form-model-item>
+
+        <a-form-model-item label="Почта:">
+          <a-input
+            v-model="form.mail"
             size="large"
             placeholder="Введите логин"
           />
@@ -26,8 +34,18 @@
           />
         </a-form-model-item>
 
+        <a-form-model-item label="Еще раз:">
+          <a-input-password
+            v-model="form.repassword"
+            size="large"
+            placeholder="Введите пароль"
+            class="auth-password"
+            style="text-align: start"
+          />
+        </a-form-model-item>
+
         <a-form-model-item style="margin-bottom: 0" :wrapper-col="{ span: 24 }">
-          <a-button size="large" block type="primary"> Войти</a-button>
+          <a-button size="large" block type="primary"> Регистрация</a-button>
         </a-form-model-item>
       </a-form-model>
     </a-card>
@@ -38,7 +56,7 @@
 import BgContainer from '@/components/BgContainer'
 
 export default {
-  name: 'Auth',
+  name: 'SignUp',
   components: { BgContainer },
   data: () => ({
     labelCol: { span: 8 },
@@ -46,6 +64,8 @@ export default {
     form: {
       login: '',
       password: '',
+      mail: '',
+      repassword: '',
     },
   }),
 }
