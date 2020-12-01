6ab653c9dc85d895417c72b6599cef6874b23497
Иван Викторович <help.venanen@yandex.ru>
2020-12-01 16:36:11 +0300

    add auth markup

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,11 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['ant-design-vue/dist/antd.css'],
+  css: [
+    'ant-design-vue/dist/antd.css',
+    '@/assets/style/flexboxgrid.css',
+    '@/assets/style/global.css',
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: ['@/plugins/antd-ui'],
--- pages/Auth.vue
+++ pages/Auth.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="auth_container">
-    <a-row>
-      <a-col :span="12"> 1 </a-col>
-      <a-col :span="12"> 1 </a-col>
-    </a-row>
+  <div>
+    <div id="auth-image-bg"></div>
+    <div class="container-fluid auth_container">
+      <div class="row h100 middle-xs center-xs">
+        <div class="col">
+          <a-card title="Авторизация" style="width: 300px">
+            <a slot="extra" href="#">Регистрация</a>
+            <div class="row"></div>
+            <p>card content</p>
+            <p>card content</p>
+          </a-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,4 +22,26 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.auth_container {
+  height: 100vh;
+  width: 100vw;
+}
+#auth-image-bg {
+  position: fixed;
+  z-index: -1;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-image: url('~assets/images/auth_bg.jpg');
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  filter: blur(2px);
+}
+.ant-card {
+  box-shadow: 4px 3px 15px 0px black;
+  border-radius: 10px;
+}
+</style>
